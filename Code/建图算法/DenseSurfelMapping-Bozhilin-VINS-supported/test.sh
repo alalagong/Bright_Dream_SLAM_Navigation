683433eb460fd9e423aab52f0b956d126c5fb39b
AilsonAir <ymtpyfyy@126.com>
2019-11-14 17:06:29 +0800

    delete

--- Code/建图算法/DenseSurfelMapping-Bozhilin-VINS-supported/test.sh
+++
@@ -1,5 +0,0 @@
-roslaunch surfel_fusion map_rviz.launch & sleep 5;
-rosrun vins vins_node '/home/dji/catkin_ws/src/VINS-Fusion/config/realsense_d435i/dji_stereo_imu_config.yaml'  & sleep 5;
-rosrun loop_fusion loop_fusion_node '/home/dji/catkin_ws/src/VINS-Fusion/config/realsense_d435i/dji_stereo_imu_config.yaml' & sleep 5;
-roslaunch surfel_fusion fuse_realsense.launch & sleep 5;
-wait
